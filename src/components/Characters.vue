ec11e1addbfa258bf9b31f345cdc52dcb6670399
Isis <isis.aguilar@social-it.cl>
2022-07-18 22:31:15 -0400

    Se visualiza Data | Filtros en proceso

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -1,32 +1,77 @@
 <template>
   <div>
-    <input type="text" />
-    <div>
-      {{ characters }}
+    <h1>{{ characters.length }}</h1>
+    <button @click="GET_CHARACTERS">click!</button>
+    <button @click="FILTER_DATA('Human')">Humano</button>
+    <button @click="FILTER_DATA('Alien')">Alien</button>
+    <div class="container">
+        <div v-for="item in characters" :key="item.id" class="tarjeta">
+        <h3>{{ item.name }}</h3>
+        <span>{{ item.species }}</span>
+        <img :src="item.image" alt="" />
     </div>
-    <button v-on:click="GET_CHARACTERS">click 1</button>
-    <button v-on:click="justShow">click</button>
+    </div>
+    
+    <select
+      name=""
+      id=""
+      v-model="filter_parameter"
+      @change="filter_parameter = item.species"
+    >
+      <option value="" v-for="item in characters" :key="item.id">
+        {{ item.species }}
+      </option>
+    </select>
   </div>
 </template>
 
 <script>
 import "../style/index.scss";
+import { mapActions, mapGetters, mapMutations, mapState } from "vuex";
 
 export default {
   name: "CharactersComponent",
-  props: ["characters"],
   // props: {
   //   msg: String,
   // },
-  methods: {
-    showCharacters() {
-      return this.$store.dispatch("GET_CHARACTERS");
-    },
-    justShow(){
-          console.log(this.characters)
-    }
+  data() {
+    return {
+      filter_parameter: "",
+    };
   },
+  methods: {
+    // showCharacters() {
+    //   return this.$store.dispatch("GET_CHARACTERS");
+    // },
+    // justShow(){
+    //       console.log(this.characters)
+    // }
+    ...mapMutations(["LOAD_CHARACTERS", "FILTER_DATA"]),
+    ...mapActions(["GET_CHARACTERS"]),
+    ...mapGetters(["quantity"]),
+  },
+  computed: {
+    ...mapState(["characters"]),
+  },
+  watch: {},
 };
 </script>
 
-<style lang="sass"></style>
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+ 
+}
+.tarjeta {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: darkseagreen;
+  margin: 1rem 0rem;
+   /* width: 230px;
+  height: 230px; */
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,29 +5,49 @@ export default createStore({
     seasons: [],
     episodes: [],
     characterName: '',
-    characters: []
+    characters: [],
+    saludos: ['hola','hello',]
   },
   getters: {
-    quantity: (state) => {  state.characters.length  }
+    quantity: (state) => {  return state.characters.length  }
   },
   mutations: {
     LOAD_CHARACTERS: (state, payload) => { state.characters = payload },
-    LOAD_CHARACTERS_NAMES: (state, payload) => { state.characterName = payload },
-    LOAD_CHARACTERS_EPISODES: (state, payload) => { state.episodes = payload }
+    // LOAD_CHARACTERS_NAMES: (state, payload) => { state.characterName = payload },
+    LOAD_CHARACTERS_EPISODES: (state, payload) => { state.episodes = payload },
+    FILTER_DATA: (state, payload) => {
+      const filtered = state.characters.filter((character) => character.species === payload )
+      state.characters = filtered;
+    }
   },
   actions: {
-    GET_CHARACTERS: (state) => {
-      fetch('https://rickandmortyapi.com/api/character')
-      .then((res) => {
-        res.json()
-      })
-      .then((response) =>{
-          const characters = response.results.map((ele) => ele)
-          const episodes = response.results.map((ele) => ele.episode)
-          state.commit("LOAD_CHARACTERS",characters)
-          state.commit("LOAD_CHARACTERS_EPISODES",episodes)
-      })
-      .catch((err) => console.log(err))
+    // GET_CHARACTERS: ({commit}) => {
+    //   fetch('https://rickandmortyapi.com/api/character')
+    //   .then((res) => {
+    //     res.json()
+    //     console.log(res)
+    //     console.log(res.json())
+    //   })
+    //   .then((response) =>{
+    //       const characters = response.results.map((ele) => ele)
+    //       console.log(characters)
+    //       commit("LOAD_CHARACTERS", characters)
+    //       // const episodes = response.results.map((ele) => ele.episode)
+    //       // console.log(episodes, characters)
+    //       // commit("LOAD_CHARACTERS_EPISODES",episodes)
+    //   })
+    //   .catch((err) => console.log(err))
+    // }
+    GET_CHARACTERS: async ({commit}) => {
+          const res = await fetch('https://rickandmortyapi.com/api/character')
+          const data = await res.json()
+          const characters = data.results
+          console.log(data)
+          console.log(characters)
+          commit("LOAD_CHARACTERS", characters)
+    },
+    GET_FILTERED_DATA: ({commit}) => {
+      commit("FILTER_DATA")
     }
   },
   modules: {
